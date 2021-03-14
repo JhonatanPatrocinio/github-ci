5b604303745af0ac74638928a3c515f775dd57bf
Jhonatan Patrocinio <jhonatan.patrocinio@gmail.com>
2021-03-13 21:20:49 -0500

    fix: add blank lines to flake8 tests pass

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def soma(a: int, b: int) -> int:
     return a + b
 
+
 def sub(a: int, b: int) -> int:
     return a - b
 
--- test_soma.py
+++ test_soma.py
@@ -4,5 +4,6 @@ from main import soma, sub
 def test_soma():
     assert soma(3, 3) == 6
 
+
 def test_sub():
     assert sub(10, 5) == 5
