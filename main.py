22e640d4564e36434a3c13eb98c7c860f20257a4
Jhonatan Patrocinio <jhonatan.patrocinio@gmail.com>
2021-03-13 21:12:46 -0500

    feat: add function sub

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 def soma(a: int, b: int) -> int:
     return a + b
 
+def sub(a: int, b: int) -> int:
+    return a - b
+
 
 if __name__ == '__main__':
     print(soma(5, 5))
+    print(sub(10, 5))
