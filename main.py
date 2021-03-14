f87baae9f8a1b37d858d9e1747119fcfe63b5c2e
Jhonatan Patrocinio <jhonatan.patrocinio@gmail.com>
2021-03-14 17:38:07 -0500

    feat: add function mult

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ def sub(a: int, b: int) -> int:
     return a - b
 
 
+def mult(a: float, b: float) -> float:
+    return a * b
+
+
 if __name__ == '__main__':
     print(soma(5, 5))
     print(sub(10, 5))
+    print(mult(5, 5))
